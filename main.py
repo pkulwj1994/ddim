75d68005f9eb7560cb1d8355b7e31509199d8b6d
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-15 19:02:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -89,11 +89,20 @@ def parse_args_and_config():
     parser.add_argument("--sequence", action="store_true")
     
     ## needed only for facades image tranfer experiments
-    parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
+    parser.add_argument('--dataset_mode', type=str, default='aligned', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
     parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
     parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
     parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
     parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
+    parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
+    parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+    parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
+    parser.add_argument('--name', type=str, default='facades_pix2pix', help='name of the experiment. It decides where to store samples and models')
+    parser.add_argument('--model', type=str, default='pix2pix', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
+    parser.add_argument('--load_size', type=int, default=286, help='scale images to this size')
+    parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
+    parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
 
 
     args = parser.parse_args()
@@ -225,7 +234,7 @@ def main():
     logging.info("Exp comment = {}".format(args.comment))
 
     try:
-        runner = Diffusion(args, config)
+        runner = DiffusionTransfer(args, config)
         if args.sample:
             runner.sample()
         elif args.test:
