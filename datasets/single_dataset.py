42c0aa164a5aace1ad2b1a79f96dd9c7eb17be49
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-15 17:49:20 +0800

    Update single_dataset.py

--- datasets/single_dataset.py
+++ datasets/single_dataset.py
@@ -1,5 +1,5 @@
-from data.base_dataset import BaseDataset, get_transform
-from data.image_folder import make_dataset
+from datasets.base_dataset import BaseDataset, get_transform
+from datasets.image_folder import make_dataset
 from PIL import Image
 
 
